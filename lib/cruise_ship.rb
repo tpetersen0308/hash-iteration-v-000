4aee2b2b9218063c7a128590c7c3cae3bc8ffae8
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 18:18:17 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,12 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
+
+winner = ""
 
 def select_winner(passengers)
   # add the code snippet here!
@@ -14,3 +16,5 @@ def select_winner(passengers)
     end
   end
 end
+
+winner
